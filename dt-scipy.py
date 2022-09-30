4a30351a3069e51c84417378c253b62ea1437aaf
ProgramMastur <gts2123@columbia.edu>
2022-09-30 16:24:08 -0400

    attempt to figure out stupid errno 13

--- dt-scipy.py
+++ dt-scipy.py
@@ -1,13 +1,14 @@
 import scipy as spicy
 import pandas as pd
 import numpy as np
+import os, sys
 
 for filename in os.listdir(f"./Data/Autosave/"):
     
     df = pd.read_csv(f"./Data/Autosave/{filename}", header=4, encoding='utf8')
     df = df.dropna(axis=1,how="all")
     
-    voltages = df["Ampl"]
+    voltages = df["Ampl"].tolist()
     
     peak_indices = spicy.findpeaks(voltages)
     
